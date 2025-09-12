c987c32654b7c04857ed16174d56e81b973dfaa3
Vlad Golubev <holybay1886@gmail.com>
2025-09-13 00:31:37 +0300

    Added Order and OrderItem mappers.

---
+++ order-service-impl/src/main/java/by/innowise/internship/orders/mapper/OrderItemMapper.java
@@ -0,0 +1,41 @@
+package by.innowise.internship.orders.mapper;
+
+import by.innowise.internship.orders.model.dto.order.OrderItemDtoRequest;
+import by.innowise.internship.orders.model.dto.order.OrderItemDtoResponse;
+import by.innowise.internship.orders.model.entity.OrderItem;
+import by.innowise.internship.orders.service.dto.ItemSnapshot;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Mapper(config = BaseMapper.class)
+public interface OrderItemMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    OrderItem toEntity(OrderItemDtoRequest dto, @Context ItemSnapshot itemSnapshot);
+
+    @AfterMapping
+    default OrderItem finishEntityMapping(@MappingTarget OrderItem orderItem,
+                                          @Context ItemSnapshot itemSnapshot) {
+        if (orderItem != null) {
+            orderItem.setId(UUID.randomUUID());
+            orderItem.setItem(itemSnapshot);
+        }
+        return orderItem;
+    }
+
+    @Mapping(source = "item.itemId", target = "itemId")
+    @Mapping(source = "item.itemName", target = "itemName")
+    @Mapping(source = "item.itemPrice", target = "itemPrice")
+    @Mapping(target = "subTotal", expression = "java(subtotal)")
+    OrderItemDtoResponse toResponseDto(OrderItem e, @Context BigDecimal subtotal);
+
+}
---
+++ order-service-impl/src/main/java/by/innowise/internship/orders/mapper/OrderMapper.java
@@ -0,0 +1,45 @@
+package by.innowise.internship.orders.mapper;
+
+import by.innowise.internship.orders.model.UserProfileDto;
+import by.innowise.internship.orders.model.dto.order.OrderCreateDto;
+import by.innowise.internship.orders.model.dto.order.OrderItemDtoResponse;
+import by.innowise.internship.orders.model.dto.order.OrderResponseDto;
+import by.innowise.internship.orders.model.entity.Order;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Mapper(config = BaseMapper.class)
+public interface OrderMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    Order toEntity(OrderCreateDto d, @Context Long userId);
+
+    @AfterMapping
+    default Order mapAdditionalProperties(@MappingTarget Order order, @Context Long userId) {
+        if (order != null) {
+            order.setId(UUID.randomUUID());
+            order.setUserId(userId);
+        }
+        return order;
+    }
+
+    @Mapping(target = "user", expression = "java(user)")
+    @Mapping(target = "orderItems", expression = "java(orderItems)")
+    @Mapping(target = "total", expression = "java(total)")
+    OrderResponseDto toDto(Order order,
+                           @Context UserProfileDto user,
+                           @Context List<OrderItemDtoResponse> orderItems,
+                           @Context BigDecimal total);
+
+}
