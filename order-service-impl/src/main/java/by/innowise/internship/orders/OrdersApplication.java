84dc46e90145fcfaa7a4d11d93a2477c682aa074
Vlad Golubev <holybay1886@gmail.com>
2025-09-14 13:32:03 +0300

    Added ConfigurationProperties scan path.

--- order-service-impl/src/main/java/by/innowise/internship/orders/OrdersApplication.java
+++ order-service-impl/src/main/java/by/innowise/internship/orders/OrdersApplication.java
@@ -2,9 +2,11 @@ package by.innowise.internship.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("by.innowise.internship.orders.config")
 @EnableFeignClients
 public class OrdersApplication {
 
