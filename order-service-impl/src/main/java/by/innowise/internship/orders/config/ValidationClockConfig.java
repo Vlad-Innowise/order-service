38928b5e723b3d5b739ddad59c24e351c4a9ba4b
Vlad Golubev <holybay1886@gmail.com>
2025-10-09 21:55:07 +0300

    Set HibernateValidator to check date in the UTC timezone.

---
+++ order-service-impl/src/main/java/by/innowise/internship/orders/config/ValidationClockConfig.java
@@ -0,0 +1,17 @@
+package by.innowise.internship.orders.config;
+
+import jakarta.validation.ClockProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+import java.time.ZoneOffset;
+
+@Configuration
+public class ValidationClockConfig {
+
+    @Bean
+    public ClockProvider utcClockProvider() {
+        return () -> Clock.system(ZoneOffset.UTC);
+    }
+}
